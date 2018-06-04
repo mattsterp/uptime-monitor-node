d7a463d96b7d47cadd203ce62ffa9fbaf697fd8d
Matthew Peters <matthewpeters@Matthews-MacBook-Pro.local>
2018-06-04 11:25:20 -0400

    Routing requests

--- index.js
+++ index.js
@@ -59,6 +59,7 @@ var server = http.createServer(function(req, res) {
       var payloadString = JSON.stringify(payload);
 
       // return the response
+      res.setHeader('Content-Type', 'application/json');
       res.writeHead(statusCode);
        // Send the resposnse
       res.end(payloadString);
